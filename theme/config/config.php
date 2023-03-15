160c655bcd86ce0eb525b1744d7ac68a6ff38688
kaestel <martin@think.dk>
2023-03-15 19:42:57 +0100

    Build

--- theme/config/config.php
+++ theme/config/config.php
@@ -3,35 +3,39 @@
 /**
 * This file contains definitions
 *
-* @package Config Dummy file
+* @package Config
 */
 header("Content-type: text/html; charset=UTF-8");
 error_reporting(E_ALL);
 
-/**
-* Site name
-*/
+define("VERSION", "0.7.9.2");
+define("UI_BUILD", "UN-BUILT");
+
 define("SITE_UID", "D2V");
 define("SITE_NAME", "Der findes en anden vej");
 define("SITE_URL", (isset($_SERVER["HTTPS"]) ? "https" : "http")."://".$_SERVER["SERVER_NAME"]);
 define("SITE_EMAIL", "os@derfindesenandenvej.dk");
 
-/**
-* Optional constants
-*/
 define("DEFAULT_PAGE_DESCRIPTION", "");
-define("DEFAULT_LANGUAGE_ISO", "EN"); // Reginal language English
-define("DEFAULT_COUNTRY_ISO", "DK"); // Regional country Denmark
+define("DEFAULT_PAGE_IMAGE", "/img/logo.png");
 
+define("DEFAULT_LANGUAGE_ISO", "EN"); 
+define("DEFAULT_COUNTRY_ISO", "DK"); 
+define("DEFAULT_CURRENCY_ISO", "DKK");
 
-// ENABLE ITEMS MODEL
+define("SITE_LOGIN_URL", "/login");
+
+define("SITE_SIGNUP", false);
+define("SITE_SIGNUP_URL", "/signup");
+
 define("SITE_ITEMS", true);
-//define("SITE_SIGNUP", true);
 
-// Enable notifications (send collection email after N notifications)
+define("SITE_SHOP", false);
+define("SHOP_ORDER_NOTIFIES", "");
+
+define("SITE_SUBSCRIPTIONS", false);
+
+define("SITE_MEMBERS", false);
+
 define("SITE_COLLECT_NOTIFICATIONS", 50);
 
-// INSTALL MODE
-//define("SITE_INSTALL", true);
-
-?>
--- theme/www/js/seg_desktop_light.js
+++ theme/www/js/seg_desktop_light.js
@@ -1,12 +1,16 @@
+/*
+asset-builder @ 2023-03-15 19:42:36
+*/
 
 /*seg_desktop_light_include.js*/
 
 /*seg_desktop_light.js*/
 if(!u || !Util) {
 	var u, Util = u = new function() {};
-	u.version = "0.9.2";
+	u.version = "0.9.3";
 	u.bug = u.nodeId = u.exception = function() {};
 	u.stats = new function() {this.pageView = function(){};this.event = function(){};}
+	u.txt = function(index) {return index;}
 }
 function fun(v) {return (typeof(v) === "function")}
 function obj(v) {return (typeof(v) === "object")}
